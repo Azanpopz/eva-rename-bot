d0b77f7a618924da07aad69c68e99d53e1e70348
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-15 21:27:42 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -19,7 +19,7 @@ INVITE_LINK = {}
 ACTIVE_CHATS = {}
 db = Database()
 
-@Bot.on_message(filters.text & filters.group & ~filters.bot, group=0)
+@Client.on_message(filters.text & filters.group & ~filters.bot, group=0)
 async def auto_filter(bot, update):
     """
     A Funtion To Handle Incoming Text And Reply With Appropriate Results
