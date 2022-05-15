981eb460637449fcc3618dba2885106a3e61889f
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-15 16:49:27 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -51,7 +51,9 @@ FILTER_MOD = {}
 #          await m.edit("**𝙰𝚄𝚃𝙾𝙵𝙸𝙻𝚃𝙴𝚁 𝙳𝙸𝚂𝙰𝙱𝙻𝙴𝙳**")
 #      else:
 #          await m.edit("𝚄𝚂𝙴 :- /autofilter on 𝙾𝚁 /autofilter off")
-
+BUTTONS = {}
+SPELL_CHECK = {}
+FILTER_MODE = {}
 @Client.on_message(filters.command('autofilter'))
 async def fil_mod(client, message): 
       mode_on = ["yes", "on", "true"]
