d6fa505b3f7903b70eea3c1ad76bcefc673738de
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-16 15:18:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
-import os
+"""Importing"""
+from os import environ
 
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-MDISK_API = os.environ.get("MDISK_API")
-CHANNEL_ID = list(int(i.strip()) for i in os.environ.get("CHANNEL_ID").split(" ")) if os.environ.get("CHANNEL_ID") else []
-FORWARD_MESSAGE = bool(os.environ.get("FORWARD_MESSAGE"))
-ADMINS = list(int(i.strip()) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("ADMINS") else []
-SOURCE_CODE = "https://github.com/kevinnadar22/URL-Shortener-V2"
-B = bool(os.environ.get("B"))
+
+class Config(object):
+    API_ID = int(environ.get("API_ID", 0))
+    API_HASH = environ.get("API_HASH", "")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "")
+    MONGO_STR = environ.get("MONGO_STR", "")
