19d2d5a39afc2658588138c0c76ca45357133aab
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-16 11:43:24 +0530

    Update filedetect.py

--- plugins/filedetect.py
+++ plugins/filedetect.py
@@ -27,7 +27,10 @@ async def refunc(client,message):
         		else:
         			markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📁 Documents",callback_data = "doc") ]])
         		# dont chenge this message.reply_text     			        		
-        		await message.reply_text(f"**Select the output file type**\n**Output FileName** :- ```{out_filename}```",reply_to_message_id=mg_id,reply_markup = markup)
+        		await message.reply_text(f"**Select the output file type**\n**Output FileName** :- {out_filename}@NASRANI_UPDATE",
+        		reply_to_message_id=mg_id,reply_markup = markup)
+        		
+",reply_to_message_id=mg_id,reply_markup = markup)
         		
         	except:
         		try:
@@ -49,6 +52,6 @@ async def refunc(client,message):
         		else:
         			markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📁 Documents",callback_data = "doc") ]])
         		# dont chenge this message.reply_text 
-        		await message.reply_text(f"**Select the output file type**\n**Output FileName** :- ```{out_filename}```",
+        		await message.reply_text(f"**Select the output file type**\n**Output FileName** :- {out_filename}@NASRANI_UPDATE",
         		reply_to_message_id=mg_id,reply_markup = markup)
         		
