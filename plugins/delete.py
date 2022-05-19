d4adc7ccc8a59c52b3bb5530e4f22fc4af411a33
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-19 22:00:38 +0530

    Update delete.py

--- plugins/delete.py
+++ plugins/delete.py
@@ -17,20 +17,7 @@ for usr in environ.get("ADMINS").split():
 START_MSG = "<b>Hai {},\nI'm a simple bot to delete group messages after a specific time</b>"
 
 
-User = Client(name="user-account",
-              session_string=SESSION,
-              api_id=API_ID,
-              api_hash=API_HASH,
-              workers=300
-              )
 
-
-Bot = Client(name="auto-delete",
-             api_id=API_ID,
-             api_hash=API_HASH,
-             bot_token=BOT_TOKEN,
-             workers=300
-             )
 
 
 @Client.on_message(filters.command('start') & filters.private)
