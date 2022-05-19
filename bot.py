c8d490dc74babe290d794c30de831a2a8b8c76c9
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-19 22:10:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,11 +17,68 @@ from utils import temp
 from pyrogram import Client
 import os
 
+
+
+
+
+#----------------------------------- https://github.com/m4mallu/DeleteMediaRobot -------------------------------------#
+
+import os
+
+from pyrogram import Client
+from user import User
+
+if bool(os.environ.get("ENV", False)):
+    from sample_config import Config
+    from sample_config import LOGGER
+else:
+    from config import Config
+    from config import LOGGER
+
+
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 API_ID = int(os.environ.get("API_ID", ""))
 
 API_HASH = os.environ.get("API_HASH", "")
+
+
+
+class Bot(Client):
+    USER: User = None
+    USER_ID: int = None
+
+    def __init__(self):
+        super().__init__(
+            "DeleteMediaRobot",
+            api_hash=Config.API_HASH,
+            api_id=Config.APP_ID,
+            bot_token=Config.TG_BOT_TOKEN,
+            plugins={
+                "root": "plugins"
+            }
+        )
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+        self.set_parse_mode("html")
+        self.LOGGER(__name__).info(
+            f"@{usr_bot_me.username}  started! "
+        )
+        self.USER, self.USER_ID = await User().start()
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped. Bye.")
+
+
+
+
+
+
+
 
 if __name__ == "__main__" :
     plugins = dict(
