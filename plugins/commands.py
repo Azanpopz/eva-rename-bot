f9af3ca3b15cfde42ba9efcdbc8ff70657541a52
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-19 20:30:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -137,12 +137,7 @@ async def start(client, message):
             if f_caption is None:
                 f_caption = f"{title}"
             try:
-              msg = await client.send_cached_media(
-                    chat_id=message.from_user.id,
-                    file_id=msg.get("file_id"),
-                    caption=f_caption,
-                    protect_content=msg.get('protect', False),
-                    )
+              
 
               msg1 = await client.send_cached_media(
                     chat_id=message.from_user.id,
