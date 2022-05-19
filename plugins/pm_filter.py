0bdbf88825d1c1128381258d3a4602a84fc53861
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-19 16:53:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -182,7 +182,7 @@ async def next_page(bot, query):
             ]
             for file in files         
 
-        ]                 
+            ]                 
                               
 
     btn.insert(0, 
