202fb5696ec2f73f8cd484945c4d6a22ac875581
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-19 22:06:51 +0530

    Update delete.py

--- plugins/delete.py
+++ plugins/delete.py
@@ -1,37 +1,49 @@
-import asyncio
-from os import environ
-from pyrogram import Client, filters, idle
+#----------------------------------- https://github.com/m4mallu/DeleteMediaRobot -------------------------------------#
 
-API_ID = int(environ.get("API_ID"))
-API_HASH = environ.get("API_HASH")
-BOT_TOKEN = environ.get("BOT_TOKEN")
-SESSION = environ.get("SESSION")
-TIME = int(environ.get("TIME"))
-AUTH_GROUP = []
-for grp in environ.get("AUTH_GROUP").split():
-    AUTH_GROUP.append(int(grp))
-ADMINS = []
-for usr in environ.get("ADMINS").split():
-    ADMINS.append(int(usr))
+import os
+import time
 
-START_MSG = "<b>Hai {},\nI'm a simple bot to delete group messages after a specific time</b>"
+from bot import Bot
+from pyrogram import filters
+from pyrogram.types import Message
+from pyrogram.errors import FloodWait
+from helper.get_messages import get_messages
+from pyrogram.errors import ChatAdminRequired
+from presets import Presets
 
+if bool(os.environ.get("ENV", False)):
+    from sample_config import Config
+else:
+    from config import Config
 
-
-
-
-@Client.on_message(filters.command('start') & filters.private)
-async def start(bot, message):
-    await message.reply(START_MSG.format(message.from_user.mention))
-
-@Client.on_message(filters.chat(AUTH_GROUP))
-async def delete(user, message):
+@Bot.on_message(filters.group & filters.command('cleanmedia'))
+async def del_all_command_fn(client: Bot, message: Message):
+    await message.delete()
+    if message.from_user.id not in Config.AUTH_USERS:
+        return
     try:
-       if message.from_user.id in ADMINS:
-          return
-       else:
-          await asyncio.sleep(TIME)
-          await Bot.delete_messages(message.chat.id, message.id)
-    except Exception as e:
-       print(e)
-       
+        status_message = await client.send_message(
+            chat_id=message.chat.id,
+            text=Presets.DELETING_MESSAGE,
+            parse_mode='html',
+            disable_web_page_preview=True
+        )
+    except ChatAdminRequired:
+        status_message = None
+    try:
+        await get_messages(
+            client.USER,
+            message.chat.id,
+            0,
+            status_message.message_id if status_message else message.message_id,
+            []
+        )
+    except FloodWait as e:
+        time.sleep(e.x)
+    await client.edit_message_text(
+        chat_id=message.chat.id,
+        text=Presets.DELETED_MESSAGE,
+        message_id=status_message.message_id,
+        parse_mode='html',
+        disable_web_page_preview=True
+    )
