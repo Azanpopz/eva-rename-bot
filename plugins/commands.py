59532c46e1afe04058591da526b685ecc19dbb0b
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-19 20:12:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -146,7 +146,7 @@ async def start(client, message):
             except FloodWait as e:
                 await asyncio.sleep(e.x)
                 logger.warning(f"Floodwait of {e.x} sec.")
-             msg1 = await client.send_cached_media(
+                msg1 = await client.send_cached_media(
                     chat_id=message.from_user.id,
                     file_id=msg.get("file_id"),
                     caption=f_caption,
