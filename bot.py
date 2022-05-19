854b3730e2a68bd092ed5e64c2c39166328bb618
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-19 22:20:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,75 +11,34 @@ from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR   
+
+
 from utils import temp
 
+
 from pyrogram import Client
 import os
 
 
+from config import *
+
+class Bot:
+    bot = Client(
+        "shortener",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        bot_token=BOT_TOKEN,
+        plugins=dict(root="plugins")
+
+    )
 
 
+TOKEN = os.environ.get("TOKEN", "")
 
-#----------------------------------- https://github.com/m4mallu/DeleteMediaRobot -------------------------------------#
-from bot import Bot
-
-import os
-
-from pyrogram import Client
-from user import User
-
-if bool(os.environ.get("ENV", False)):
-    from sample_config import Config
-    from sample_config import LOGGER
-else:
-    from config import Config
-    from config import LOGGER
-
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-
-API_ID = int(os.environ.get("API_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", ""))
 
 API_HASH = os.environ.get("API_HASH", "")
-
-
-
-class Bot(Client):
-    USER: User = None
-    USER_ID: int = None
-
-    def __init__(self):
-        super().__init__(
-            "DeleteMediaRobot",
-            api_hash=Config.API_HASH,
-            api_id=Config.APP_ID,
-            bot_token=Config.TG_BOT_TOKEN,
-            plugins={
-                "root": "plugins"
-            }
-        )
-        self.LOGGER = LOGGER
-
-    async def start(self):
-        await super().start()
-        usr_bot_me = await self.get_me()
-        self.set_parse_mode("html")
-        self.LOGGER(__name__).info(
-            f"@{usr_bot_me.username}  started! "
-        )
-        self.USER, self.USER_ID = await User().start()
-
-    async def stop(self, *args):
-        await super().stop()
-        self.LOGGER(__name__).info("Bot stopped. Bye.")
-
-
-
-
-
-
-
 
 if __name__ == "__main__" :
     plugins = dict(
@@ -87,12 +46,15 @@ if __name__ == "__main__" :
     )
     app = Client(
         "renamer",
-        bot_token=BOT_TOKEN,
-        api_id=API_ID,
+        bot_token=TOKEN,
+        api_id=APP_ID,
         api_hash=API_HASH,
         plugins=plugins
     )
-        
+
+
+
+
 
 
 class Bot(Client):
@@ -108,7 +70,7 @@ class Bot(Client):
             sleep_threshold=5,
             parse_mode="html",
         )
-
+    
     async def start(self):
         b_users, b_chats = await db.get_banned()
         temp.BANNED_USERS = b_users
@@ -129,6 +91,5 @@ class Bot(Client):
         logging.info("Bot stopped. Bye.")
 
 
-
-
-Bot().run()
+app = Bot()
+app.run()
