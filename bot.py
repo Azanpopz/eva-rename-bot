cd806ff390e57cbb5ff1370fb66bdce0c83cc432
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-20 13:26:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,6 +23,31 @@ import os
 
 from config import *
 
+from pyrogram import Client
+import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+API_ID = int(os.environ.get("API_ID", ""))
+
+API_HASH = os.environ.get("API_HASH", "")
+
+if __name__ == "__main__" :
+    plugins = dict(
+        root="plugins"
+    )
+    app = Client(
+        "renamer",
+        bot_token=BOT_TOKEN,
+        api_id=API_ID,
+        api_hash=API_HASH,
+        plugins=plugins
+    )
+
+
+
+
+
 class Bot:
     bot = Client(
         "shortener",
